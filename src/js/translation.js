2fce8b57b95f0a33ac864cf00b8a4f678f6a101e
Ulinator <zahuta@gmail.com>
2019-08-11 15:55:12 +0200

    Rewritten the data model

---
+++ src/js/translation.js
@@ -0,0 +1,11 @@
+const translation = {
+    "cool": ["chłodna", "chłodny", "chłodnym"],
+    "neutral": ["neutralna", "neutralny", "neutralnym"],
+    "warm": ["ciepła", "ciepły", "ciepłym"],
+    "pink": ["różowa", "różowy", "różowym"],
+    "blue": ["niebieska", "niebieski", "niebieskim"],
+    "green": ["oliwkowa", "oliwkowy", "oliwkowym"],
+    "yellow": ["złota", "złoty", "złotym"],
+    "orange": ["brzosk-winiowa", "brzosk-winiowy", "brzoskwiniowym"],
+    "red": ["czerwona", "czerwony", "czerwonym"]
+};
